0b30e607dffb6c68155fb4280f111aa13d98a22f
nabychan <nabychan@hotmail.com>
2016-08-23 01:49:39 +0800

    基本功能完成
    基本功能完成

--- CpuMonitor/App.xaml.cs
+++ CpuMonitor/App.xaml.cs
@@ -5,6 +5,13 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Threading;
+using Forms = System.Windows.Forms;
+using System.Management;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Text;
+using System.IO;
 
 namespace CpuMonitor
 {
@@ -13,5 +20,69 @@ namespace CpuMonitor
     /// </summary>
     public partial class App : Application
     {
+        private Forms.NotifyIcon _NotifyIcon;
+        private string _IconPath = @"..\..\CpuMonitor.ico";
+        private CpuWatcher _Watcher;
+        private Bitmap _State;
+        public Forms.NotifyIcon NotifyIcon { get { return _NotifyIcon; } }
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var createNew = false;
+            var mutex = new Mutex(false, "", out createNew);
+            if (!createNew)
+            {
+                MessageBox.Show("已经在运行！");
+                Shutdown();
+            }
+            else
+            {
+                _NotifyIcon = new Forms.NotifyIcon();
+                _NotifyIcon.Icon = new Icon(_IconPath);
+                _NotifyIcon.Text = "Cpu占用率(%)";
+
+                _State = new Bitmap(@"..\..\Monitor.bmp"); ;
+
+                var contextMenu = new Forms.ContextMenu();
+
+                var exitMenuItem = new Forms.MenuItem();
+                exitMenuItem.Text = "退出";
+                exitMenuItem.Click += new EventHandler(delegate { Shutdown(); });
+
+                contextMenu.MenuItems.Add(exitMenuItem);
+
+                _NotifyIcon.ContextMenu = contextMenu;
+                _NotifyIcon.Visible = true;
+
+                _Watcher = new CpuWatcher();
+                _Watcher.Watch(2000);
+            }
+        }
+
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            if (_Watcher != null)
+            {
+                _Watcher.Dispose();
+            }
+            if (_NotifyIcon != null)
+            {
+                _NotifyIcon.Visible = false;
+                _NotifyIcon.Dispose();
+                _NotifyIcon = null;
+            }            
+        }
+
+        public void Refresh(int percentage)
+        {
+            using (var g = Graphics.FromImage(_State))
+            {
+                g.Clear(Color.Transparent);
+                g.DrawString(percentage.ToString(), new Font("Arial", 200, GraphicsUnit.Pixel), Brushes.White, new PointF(0, 0));
+            }
+            var icon = Icon.FromHandle(_State.GetHicon());
+            _NotifyIcon.Icon = icon;
+            _NotifyIcon.Visible = false;
+            _NotifyIcon.Visible = true;
+        }
     }
 }
---
+++ CpuMonitor/CpuWatcher.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Threading;
+using System.Threading;
+using System.Windows;
+using System.Runtime.InteropServices;
+using System.Management;
+
+namespace CpuMonitor
+{   
+    public class CpuWatcher : IDisposable
+    {
+        private PerformanceCounter _PerformanceCounter;
+        private bool _Watch;
+        private Task _Task;
+        #region AIP声明 
+        [DllImport("IpHlpApi.dll")]
+        extern static public uint GetIfTable(byte[] pIfTable, ref uint pdwSize, bool bOrder);
+
+        [DllImport("User32")]
+        private extern static int GetWindow(int hWnd, int wCmd);
+
+        [DllImport("User32")]
+        private extern static int GetWindowLongA(int hWnd, int wIndx);
+
+        [DllImport("user32.dll")]
+        private static extern bool GetWindowText(int hWnd, StringBuilder title, int maxBufSize);
+
+        [DllImport("user32", CharSet = CharSet.Auto)]
+        private extern static int GetWindowTextLength(IntPtr hWnd);
+        #endregion
+        public CpuWatcher()
+        {
+            _PerformanceCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            _PerformanceCounter.MachineName = ".";
+            _PerformanceCounter.NextValue();
+            _Watch = false;
+        }
+
+        public void Watch(int seconds)
+        {
+            _Watch = true;
+            _Task = new TaskFactory().StartNew(() =>
+              {
+                  while (_Watch)
+                  {
+                      (App.Current as App).Refresh((int)_PerformanceCounter.NextValue());
+                      //(App.Current as App).NotifyIcon.BeginInvoke(
+                      //    DispatcherPriority.Background,
+                      //    new Action<int>(p =>
+                      //    {
+                      //        App.Current.Shutdown();
+                      //        MessageBox.Show("sfsd");
+                      //        //(App.Current as App).Refresh(p);
+                      //    }),
+                      //    _PerformanceCounter.NextValue());
+                      System.Threading.Thread.Sleep(seconds);
+                  }
+              });
+        }
+        public void StopWatch()
+        {
+            _Watch = false;
+            if (_Task != null)
+            {
+                _Task.Wait();
+                _Task = null;
+            }
+        }
+
+        public void Dispose()
+        {
+
+            StopWatch();
+            _PerformanceCounter.Dispose();
+        }
+    }
+}
